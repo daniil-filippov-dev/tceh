2d3b0881c7bb9a2063e42dd4bd4231157678f612
danil_lans <danimen0001@gmail.com>
2020-03-24 14:21:56 +0000

    added day 1 included types, sizes, vars

---
+++ day_1/everything_is_an_object.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+import sys  # importing the standart library `sys`.
+
+
+print(sys.version_info)
+print('int is object: ', isinstance(3, object))
+print('True is object: ', isinstance(True, object))
+print('None is object: ', isinstance(None, object))
+print('isinstance is object: ', isinstance(isinstance, object))
